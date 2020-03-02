a81a0fa1bfaaeff6eaa2e7b1207952d9c504f8f2
webees <webees@qq.com>
2020-03-02 12:51:28 +0800

    Fix extended type enumeration not working.

---
+++ src/Protobuf.ts
@@ -0,0 +1,32 @@
+import pbJS from 'protobufjs'
+import Logs from './Logs'
+
+declare global {
+  interface JsonFile extends pbJS.INamespace { }
+}
+
+export default class Protobuf {
+  __ROOT__: any
+  constructor(json: JsonFile) {
+    this.__ROOT__ = pbJS.Root.fromJSON(json)
+  }
+
+  async encode(type: string, data = {}) {
+    const pb = this.__ROOT__.lookupTypeOrEnum(type)
+    const msg = pb.fromObject(data)
+    Logs.add('encode', { type, data })
+    return pb.encode(msg).finish()
+  }
+
+  async decode(type: string, data: Buffer) {
+    const msg = this.__ROOT__.lookupTypeOrEnum(type).decode(data).toJSON()
+    Logs.add('decode', { type, data: msg })
+    return { type, data: msg }
+  }
+
+  async lookupEnum(path: string, key: number | string) {
+    const types = this.__ROOT__.lookupEnum(path)
+    if (typeof key === 'number') return types.valuesById[key]
+    if (typeof key === 'string') return types.values[key]
+  }
+}
