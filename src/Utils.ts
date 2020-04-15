4b43419487f4f1989ba1d1c8d893b8dd5ffb6fcc
webees <webees@qq.com>
2020-04-15 17:37:55 +0800

    add ethereumjs

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,5 +1,6 @@
 import * as Bip32 from 'bip32'
 import { Transaction } from 'ethereumjs-tx'
+import { BN } from 'ethereumjs-util'
 
 export default class Utils {
   static HD_HARDENED: number = 0x80000000
@@ -20,42 +21,26 @@ export default class Utils {
     }
   }
 
-  static getScriptType = (path?: Array<number>) => {
-    if (!Array.isArray(path) || path.length < 1) return 'SPENDADDRESS'
-    const p1 = Utils.fromHardened(path[0])
-    switch (p1) {
-      case 48:
-        return 'SPENDMULTISIG';
-      case 49:
-        return 'SPENDP2SHWITNESS';
-      case 84:
-        return 'SPENDWITNESS';
-      default:
-        return 'SPENDADDRESS';
-    }
-  }
-
-  static padToEven = (val: string) => {
-    if (/^[0-9]+$/.test(val)) val = parseInt(val).toString(16)
-    if (val.length % 2) val = `0${val}`
-    return val
+  static int2hex = (num: BN) => {
+    let res = new BN(num).toString(16)
+    if (res.length % 2 !== 0) res = `0${res}`
+    return res
   }
 
   static ethTx = (params: any, rsv: any) => {
-    const r = Buffer.from(rsv.r, 'base64')
-    const s = Buffer.from(rsv.s, 'base64')
-    const v = Utils.padToEven(rsv.v)
+    const r = Buffer.from(rsv.r, 'base64').toString('hex')
+    const s = Buffer.from(rsv.s, 'base64').toString('hex')
+    const v = Utils.int2hex(rsv.v)
     const tx = {
-      nonce: '0x' + params.nonce.toString('hex'),
-      gasPrice: '0x' + params.gas_price.toString('hex'),
-      gasLimit: '0x' + params.gas_limit.toString('hex'),
-      to: params.to.toString('hex'),
-      value: '0x' + params.value.toString('hex'),
-      r: '0x' + r.toString('hex'),
-      s: '0x' + s.toString('hex'),
-      v: '0x' + v
+      to: params.to,
+      value: `0x${params.value}`,
+      nonce: `0x${params.nonce}`,
+      gasPrice: `0x${params.gas_price}`,
+      gasLimit: `0x${params.gas_limit}`,
+      r: `0x${r}`,
+      s: `0x${s}`,
+      v: `0x${v}`
     }
-    console.log(tx)
-    return '0x' + (new Transaction(tx).serialize().toString('hex'))
+    return `0x${(new Transaction(tx).serialize().toString('hex'))}`
   }
 }
